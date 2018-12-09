8f64d32d35826327df82016992894e19287d13e1
Simon Schneegans <code@simonschneegans.de>
2018-12-09 23:15:18 +0100

    add simple Commandline parser

---
+++ src/Illusion/Core/CommandLineOptions.cpp
@@ -0,0 +1,127 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                //
+//   _)  |  |            _)                 This software may be modified and distributed         //
+//    |  |  |  |  | (_-<  |   _ \    \      under the terms of the MIT license.                   //
+//   _| _| _| \_,_| ___/ _| \___/ _| _|     See the LICENSE file for details.                     //
+//                                                                                                //
+//  Authors: Simon Schneegans (code@simonschneegans.de)                                           //
+//                                                                                                //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "CommandLineOptions.hpp"
+
+#include "Logger.hpp"
+
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+
+namespace Illusion::Core {
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+CommandLineOptions::CommandLineOptions(std::string const& description)
+  : mDescription(description) {}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void CommandLineOptions::addOption(
+  std::vector<std::string> const& optionNames, OptionValue value, std::string const& help) {
+
+  mOptions.emplace_back(Option{optionNames, value, help});
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void CommandLineOptions::printHelp() const {
+  ILLUSION_MESSAGE << mDescription << std::endl;
+
+  uint32_t maxNameLength = 0;
+
+  for (auto const& option : mOptions) {
+    uint32_t nameLength = 0;
+    for (auto const& name : option.mNames) {
+      nameLength += name.size() + 2;
+    }
+
+    maxNameLength = std::max(maxNameLength, nameLength);
+  }
+
+  for (auto const& option : mOptions) {
+
+    std::string names;
+
+    for (auto const& name : option.mNames) {
+      names += name + ", ";
+    }
+
+    std::stringstream sstr;
+    sstr << std::left << std::setw(maxNameLength) << names.substr(0, names.size() - 2)
+         << option.mHelp;
+
+    ILLUSION_MESSAGE << sstr.str() << std::endl;
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void CommandLineOptions::parse(int argc, char* argv[]) const {
+  int i = 1;
+  while (i < argc) {
+    std::string name(argv[i]);
+    std::string value;
+
+    size_t equalPos = name.find('=');
+    if (equalPos != std::string::npos) {
+      value = name.substr(equalPos + 1);
+      name  = name.substr(0, equalPos);
+    }
+
+    if (i + 1 < argc && argv[i + 1][0] != '-') {
+      value = argv[i + 1];
+      ++i;
+    }
+
+    bool foundOption = false;
+
+    for (auto const& option : mOptions) {
+      if (std::find(option.mNames.begin(), option.mNames.end(), name) != option.mNames.end()) {
+        foundOption = true;
+
+        if (std::holds_alternative<bool*>(option.mValue)) {
+          if (value.size() > 0 && value != "true" && value != "false") {
+            throw std::runtime_error(
+              "Failed to parse command line arguments: Value for bool type option \"" + name +
+              "\" must be empty, true or false!");
+          }
+          *std::get<bool*>(option.mValue) = (value != "false");
+        } else if (value == "") {
+          throw std::runtime_error(
+            "Failed to parse command line arguments: Missing value for option \"" + name + "\"!");
+        } else if (std::holds_alternative<std::string*>(option.mValue)) {
+          *std::get<std::string*>(option.mValue) = value;
+        } else {
+          std::visit(
+            [&value](auto&& arg) {
+              std::stringstream sstr(value);
+              sstr >> *arg;
+            },
+            option.mValue);
+        }
+
+        break;
+      }
+    }
+
+    if (!foundOption) {
+      ILLUSION_WARNING << "Ignoring unknown command line option \"" << name << "\"." << std::endl;
+    }
+
+    ++i;
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+} // namespace Illusion::Core
---
+++ src/Illusion/Core/CommandLineOptions.hpp
@@ -0,0 +1,51 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                //
+//   _)  |  |            _)                 This software may be modified and distributed         //
+//    |  |  |  |  | (_-<  |   _ \    \      under the terms of the MIT license.                   //
+//   _| _| _| \_,_| ___/ _| \___/ _| _|     See the LICENSE file for details.                     //
+//                                                                                                //
+//  Authors: Simon Schneegans (code@simonschneegans.de)                                           //
+//                                                                                                //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef ILLUSION_CORE_COMMAND_LINE_OPTIONS_HPP
+#define ILLUSION_CORE_COMMAND_LINE_OPTIONS_HPP
+
+#include <string>
+#include <variant>
+#include <vector>
+
+namespace Illusion::Core {
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class CommandLineOptions {
+
+ public:
+  typedef std::variant<int*, double*, float*, bool*, std::string*> OptionValue;
+
+  explicit CommandLineOptions(std::string const& description);
+
+  void addOption(
+    std::vector<std::string> const& optionNames, OptionValue value, std::string const& help);
+
+  void printHelp() const;
+
+  void parse(int argc, char* argv[]) const;
+
+ private:
+  std::string mDescription;
+
+  struct Option {
+    std::vector<std::string> mNames;
+    OptionValue              mValue;
+    std::string              mHelp;
+  };
+
+  std::vector<Option> mOptions;
+};
+
+} // namespace Illusion::Core
+
+#endif // ILLUSION_CORE_COMMAND_LINE_OPTIONS_HPP
