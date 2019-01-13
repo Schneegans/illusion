40cb56fc4954a03dc89d1efabf69ad8e38a6093d
Simon Schneegans <code@simonschneegans.de>
2019-01-13 21:37:54 +0100

    :sparkles: Change formatting of copyright header

--- externals/gli.cmake
+++ externals/gli.cmake
@@ -1,8 +1,8 @@
 # ------------------------------------------------------------------------------------------------ #
 #                                                                                                  #
-#    _)  |  |            _)               This code may be used and modified under the terms       #
-#     |  |  |  |  | (_-<  |   _ \    \    of the MIT license. See the LICENSE file for details.    #
-#    _| _| _| \_,_| ___/ _| \___/ _| _|   Copyright (c) 2018-2019 Simon Schneegans                 #
+#     _)  |  |            _)                This code may be used and modified under the terms     #
+#      |  |  |  |  | (_-<  |   _ \    \     of the MIT license. See the LICENSE file for details.  #
+#     _| _| _| \_,_| ___/ _| \___/ _| _|    Copyright (c) 2018-2019 Simon Schneegans               #
 #                                                                                                  #
 # ------------------------------------------------------------------------------------------------ #
 
