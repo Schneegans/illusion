0227adfa087d822eba77970688dd1f6496f34829
Simon Schneegans <code@simonschneegans.de>
2019-02-24 14:48:53 +0100

    :tada: Add test runner

---
+++ test/main.cpp
@@ -0,0 +1,14 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                //
+//    _)  |  |            _)                This code may be used and modified under the terms    //
+//     |  |  |  |  | (_-<  |   _ \    \     of the MIT license. See the LICENSE file for details. //
+//    _| _| _| \_,_| ___/ _| \___/ _| _|    Copyright (c) 2018-2019 Simon Schneegans              //
+//                                                                                                //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest.h>
+
+#include <Illusion/Core/BitHash.hpp>
+#include <Illusion/Core/Color.hpp>
+#include <Illusion/Core/Property.hpp>
