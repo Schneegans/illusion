7ebec401152b7768e4b6ab0abce5e9eaa73cbea8
Simon Schneegans <code@simonschneegans.de>
2019-02-27 06:48:22 +0100

    :memo: Add some comments to lcov.sh

--- lcov.sh
+++ lcov.sh
@@ -8,18 +8,33 @@
 #                                                                                                  #
 # ------------------------------------------------------------------------------------------------ #
 
-# get the location of this script
+# This script uses lcov to capture the source coverage of the test executed with RunAllTests.sh. If
+# you pass any argument to the script (say ./lcov.sh foo) then it will create also an html report
+# and open this report in your web browser.
+
+# Get the location of this script.
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd $SCRIPT_DIR
 
+# Create initial zero-coverage report.
 lcov -q --zerocounters --directory .
 lcov -q --capture --no-external --initial --directory . --output-file build/zero_coverage.info
 
+# Run all tests
 build/install/bin/RunAllTests.sh
 
+# Capture the coverage of the test.
 lcov -q --capture --no-external --directory . --output-file build/test_coverage.info
 lcov -q -a build/zero_coverage.info -a build/test_coverage.info --o build/coverage.info
 
+# Remove any coverage from externals, examples and test directories.
 lcov -q --remove build/coverage.info \*externals/\* --output-file build/coverage.info
 lcov -q --remove build/coverage.info \*examples/\* --output-file build/coverage.info
 lcov -q --remove build/coverage.info \*test/\* --output-file build/coverage.info
+
+# Generate html report and open it in a web browser when an argument was passed to the script.
+if [ $# != 0 ]
+  then
+    genhtml build/coverage.info --output-directory build/coverage
+    xdg-open build/coverage/index.html
+fi
