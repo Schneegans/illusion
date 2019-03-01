30a5db9d9e48517b60c51677b85c29e00b27a227
Simon Schneegans <code@simonschneegans.de>
2019-03-01 21:26:02 +0100

    :wrench: Use moving average in FPSCounter

--- src/Illusion/Core/FPSCounter.hpp
+++ src/Illusion/Core/FPSCounter.hpp
@@ -9,37 +9,40 @@
 #ifndef ILLUSION_CORE_FPS_COUNTER_HPP
 #define ILLUSION_CORE_FPS_COUNTER_HPP
 
+#include "MovingAverage.hpp"
 #include "Property.hpp"
 #include "Timer.hpp"
 
 namespace Illusion::Core {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+// The FPSCounter can be used to measure the average frame time and frames per second of the last //
+// C frames. Just call step() once each frame. Internally, a MovingAverage is used to calculate   //
+// the values.                                                                                    //
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+template <size_t C>
 class FPSCounter {
 
  public:
-  // This property contains the current frames per seconds.
-  Float pFPS = 0.f;
+  // This property contains the average frames per seconds of the last C frames.
+  Double pFPS = 0.0;
 
-  // Every t frames the Fps property is updated.
-  explicit FPSCounter(unsigned t = 100);
-
-  // Call this after creation of this counter.
-  void start();
+  // This property contains the average frames time of the last C frames.
+  Double pFrameTime = 0.0;
 
   // Call this once a frame.
-  void step();
-
-  // returns the number of times step() has been called
-  unsigned getFrameCount() const;
+  void step() {
+    mAverage.add(mTimer.restart());
+    pFrameTime = mAverage.get();
+    pFPS       = 1.0 / pFrameTime.get();
+  }
 
  private:
-  unsigned mFrameCount = 0;
-  unsigned mDelay      = 10;
-  Timer    mTimer;
+  MovingAverage<double, C> mAverage;
+  Timer                    mTimer;
 };
+
 } // namespace Illusion::Core
 
 #endif // ILLUSION_CORE_FPS_COUNTER_HPP
